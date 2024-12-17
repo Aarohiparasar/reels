03c68ef09f294e6086eede5a464dd6a0c1b5e821
aarohi singh <aarohisinghparasar29@gmail.com>
2024-12-17 13:20:04 +0530

    updated

--- components/Upload.js
+++ components/Upload.js
@@ -1,32 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "@mui/joy/Button";
-import MovieIcon from '@mui/icons-material/Movie';
+import MovieIcon from "@mui/icons-material/Movie";
+import LinearProgress from "@mui/material/LinearProgress";
 
-import LinearProgress from '@mui/material/LinearProgress';
+function Upload({ onVideoUpload }) {
+  const [progress, setProgress] = useState(0);
 
-function Upload() {
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const videoUrl = URL.createObjectURL(file);
+      onVideoUpload(videoUrl);
+      simulateProgress();
+    }
+  };
+
+  const simulateProgress = () => {
+    let value = 0;
+    const interval = setInterval(() => {
+      value += 20;
+      setProgress(value);
+      if (value >= 100) clearInterval(interval);
+    }, 200);
+  };
+
   return (
     <div className="upload-btn">
       <Button
         className="upload"
         variant="outlined"
-        startIcon={<MovieIcon/>}
+        startIcon={<MovieIcon />}
         fullWidth
         component="label"
         style={{ marginTop: "1rem" }}
       >
+        Upload Video
         <input
           type="file"
-          accept="image/*"
+          accept="video/*"
           style={{ display: "none" }}
+          onChange={handleFileChange}
         />
-        Upload
       </Button>
-      <LinearProgress variant="determinate" value={10} 
-      style={{marginTop:"0.2rem"}}/>
+      {progress > 0 && (
+        <LinearProgress
+          variant="determinate"
+          value={progress}
+          style={{ marginTop: "0.5rem" }}
+        />
+      )}
     </div>
   );
 }
 
 export default Upload;
-
--- components/feed.js
+++ components/feed.js
@@ -1,37 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./navbar";
 import Upload from "./Upload";
 import Avatar from "@mui/material/Avatar";
 import FavoriteIcon from "@mui/icons-material/Favorite";
+
 function Feed() {
+  const [videos, setVideos] = useState([
+    {
+      id: 1,
+      url: "https://ik.imagekit.io/ikmedia/videodemo/o/videos%2Ftest-video.mp4?tr=w-400,h-640&alt=media&token=044352ec-306d-48f9-a93f-a6b034764eac",
+      username: "User One",
+      likes: 25,
+    },
+    {
+      id: 2,
+      url: "https://www.w3schools.com/html/mov_bbb.mp4",
+      username: "User Two",
+      likes: 15,
+    },
+  ]);
+
+  const handleVideoUpload = (videoUrl) => {
+    const newVideo = {
+      id: videos.length + 1,
+      url: videoUrl,
+      username: "New User",
+      likes: 0,
+    };
+    setVideos((prevVideos) => [newVideo, ...prevVideos]);
+  };
+
   return (
     <div className="feed-container">
       <Navbar />
-      <Upload />
+      <Upload onVideoUpload={handleVideoUpload} />
       <div className="videos-container">
-        <div className="post-container">
-          <video src="https://ik.imagekit.io/ikmedia/videodemo/o/videos%2Ftest-video.mp4?tr=w-400,h-640&alt=media&token=044352ec-306d-48f9-a93f-a6b034764eac"/>
-          <div className="video-info">
-          <div className="avatar-container">
-            <Avatar
-              alt="Remy Sharp"
-              src="/static/images/avatar/2.jpg"
-              sx={{ margin: "0.5rem" }}
+        {videos.map((video) => (
+          <div
+            key={video.id}
+            className="post-container"
+            style={{ margin: "1rem 0" }}
+          >
+            <video
+              src={video.url}
+              controls
+              width="100%"
+              style={{ borderRadius: "10px", backgroundColor: "#000" }}
             />
-           <p>Name</p>
+            <div
+              className="video-info"
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                marginTop: "0.5rem",
+              }}
+            >
+              <div
+                className="avatar-container"
+                style={{ display: "flex", alignItems: "center" }}
+              >
+                <Avatar alt={video.username} src="" sx={{ margin: "0.5rem" }} />
+                <p style={{ marginLeft: "0.5rem" }}>{video.username}</p>
+              </div>
+              <div
+                className="post-like"
+                style={{ display: "flex", alignItems: "center" }}
+              >
+                <FavoriteIcon fontSize="large" />
+                <p style={{ marginLeft: "0.5rem" }}>{video.likes}</p>
+              </div>
+            </div>
           </div>
-          <div className="post-like">
-          <FavoriteIcon fontSize="large"/>
-          <p style={{marginLeft:"0.5rem"}}>10</p>
-          </div>
-        </div>
-        </div>
-       
-        <div className="post-container">
-          <video />
-        </div>
+        ))}
       </div>
     </div>
   );
 }
+
 export default Feed;
